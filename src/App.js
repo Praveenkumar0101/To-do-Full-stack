f6a9ec2e9c6b0b826671f69064d50f6803621fef
Praveenkumar0101 <praveenkumar.vancharla@gmail.com>
2024-10-09 17:07:28 +0530

    backend file added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+// // App.js
+// import React from 'react';
+// import { BrowserRouter as Router, Route, Routes, NavLink, Navigate } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// import Dashboard from './Dashboard'; // Import the Dashboard component
+// import './App.css';
+// import TaskManager from './taskManager';
+
+// function App() {
+//   return (
+//     <Router>
+//       <div className="container">
+//         <div className="sidebar">
+//           <h2>To-do</h2>
+//           <div className="logo">
+//             <img src="path_to_your_logo.png" alt="Logo" /> {/* Update with the path to your logo */}
+//           </div>
+          
+//           <nav>
+//             <ul>
+//               <li>
+//                 <NavLink className='color' to="/dashboard" activeClassName="active" >Dashboard</NavLink>
+//               </li>
+//               <li>
+//                 <NavLink className='color' to="/taskManager" activeClassName="active">Categories</NavLink>
+//               </li>
+//               {/* <li> */}
+//                 {/* <NavLink className='color' to="/sub-categories" activeClassName="active">Sub-categories</NavLink>
+//               </li>
+//               <li>
+//                 <NavLink className='color' to="/products" activeClassName="active">Products</NavLink>
+//               </li> */}
+//             </ul>
+//           </nav>
+//         </div>
+//         <div className="main-content">
+//           <Routes>
+//             <Route path="/dashboard" element={<Dashboard />} />
+//             <Route path="/taskManager" element={<TaskManager />} />
+//             {/* <Route path="/sub-categories" element={<SubCategoryManager />} />
+//             <Route path="/products" element={<ProductManager />} /> */}
+//             <Route path="/" element={<Navigate to="/dashboard" replace />} />
+//           </Routes>
+//         </div>
+//       </div>
+//     </Router>
+//   );
+// }
+
+// export default App;
+
+import TaskManager from './taskManager';
+function App(){
+  return(
+    <>
+    <TaskManager/>
+    </>
+  )
 }
 
 export default App;
